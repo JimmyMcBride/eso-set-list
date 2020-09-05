694f51719ee13647eea13550b94686289af5a926
JimmyMcBride <mcbride967@gmail.com>
2020-09-05 00:50:04 -0500

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "sriracha-ui/css/main.css";
+import "./styles.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
